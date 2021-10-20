5ceed3d4244ecac18e1fb7ccf08ffa78a8d7481a
Manos Kampourakis <ekampourakis@ceid.upatras.gr>
2021-10-20 15:58:54 +0300

    implement request

--- include/wrapper.hpp
+++ include/wrapper.hpp
@@ -1,7 +1,11 @@
-#include <ros/ros.h>
+#include <algorithm>
 
+#include <HTTPRequest.hpp>
+
+#include <ros/ros.h>
 #include <wled_ros_wrapper/WLED.h>
 
+ros::NodeHandle* nh_ptr;
 ros::Subscriber subscriber;
 
 void topic_callback(const wled_ros_wrapper::WLED::ConstPtr& msg);
--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -2,16 +2,39 @@
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "wled_ros_wrapper");
-
-    ros::NodeHandle nh;
+    ros::NodeHandle nh("wled_ros_wrapper");
+    nh_ptr = &nh;
 
     subscriber = nh.subscribe("set", 1, topic_callback);
 
     ros::spin();
-
     return 0;
 }
 
 void topic_callback(const wled_ros_wrapper::WLED::ConstPtr& msg) {
-    ROS_INFO("Got a new request");
+    try {
+        // Load the address parameter from the ROS parameter server
+        std::string address;
+        nh_ptr->param<std::string>("address", address, "wled.local");
+       
+        // Create the base link of the request
+        std::string request = "http://" + address + "/win";
+
+        // Populate the request's fields
+        request += "&T=" + msg->state ? "1" : "0";
+        request += "&A=" + std::to_string(msg->brightness);
+        request += "&R=" + std::to_string(std::clamp<char>(msg->color.r, 0, 255));
+        request += "&G=" + std::to_string(std::clamp<char>(msg->color.g, 0, 255));
+        request += "&B=" + std::to_string(std::clamp<char>(msg->color.b, 0, 255));
+        request += "&W=" + std::to_string(std::clamp<char>(msg->color.a, 0, 255));
+        request += "&FX=" + std::to_string(msg->effect.type);
+        request += "&SX=" + std::to_string(msg->effect.speed);
+        request += "&IX=" + std::to_string(msg->effect.intensity);
+
+        // Send the GET request
+        http::Request get{request};
+        get.send("GET");
+    } catch (const std::exception& e) {
+        ROS_ERROR("Request failed, error: %s", e.what());
+    }
 }
