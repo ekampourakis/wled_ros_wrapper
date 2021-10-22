5dc81dd4a209d4fab93c53713e806d464147d15f
ShareWork Cell <bakopoulou@lms.mech.upatras.gr>
2021-10-22 14:01:32 +0300

    simplify state change

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -5,6 +5,11 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh("wled_ros_wrapper");
     nh_ptr = &nh;
 
+    // Set the ROS logging level (for better debugging)
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
+
     subscriber = nh.subscribe("set", 1, topic_callback);
 
     ros::spin();
@@ -16,20 +21,24 @@ void topic_callback(const wled_ros_wrapper::WLED::ConstPtr& msg) {
         // Load the address parameter from the ROS parameter server
         std::string address;
         nh_ptr->param<std::string>("address", address, "wled.local");
-       
+
         // Create the base link of the request
         std::string request = "http://" + address + "/win";
 
         // Populate the request's fields
-        request += "&T=" + msg->state ? "1" : "0";
-        request += "&A=" + std::to_string(msg->brightness);
-        request += "&R=" + std::to_string(std::clamp<char>(msg->color.r, 0, 255));
-        request += "&G=" + std::to_string(std::clamp<char>(msg->color.g, 0, 255));
-        request += "&B=" + std::to_string(std::clamp<char>(msg->color.b, 0, 255));
-        request += "&W=" + std::to_string(std::clamp<char>(msg->color.a, 0, 255));
-        request += "&FX=" + std::to_string(msg->effect.type);
-        request += "&SX=" + std::to_string(msg->effect.speed);
-        request += "&IX=" + std::to_string(msg->effect.intensity);
+        if (msg->state) {
+            request += "&T=1";
+            request += "&A=" + std::to_string(msg->brightness);
+            request += "&R=" + std::to_string(msg->r);
+            request += "&G=" + std::to_string(msg->g);
+            request += "&B=" + std::to_string(msg->b);
+            request += "&W=" + std::to_string(msg->a);
+            request += "&FX=" + std::to_string(msg->effect.type);
+            request += "&SX=" + std::to_string(msg->effect.speed);
+            request += "&IX=" + std::to_string(msg->effect.intensity);
+        } else {
+            request += "&T=0";
+        }
 
         // Send the GET request
         http::Request get{request};
